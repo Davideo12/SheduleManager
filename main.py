ed0ebf2f52e1de4d7161f9fd85cde7ab198ed2b5
Davideo12 <j.davideo12@gmail.com>
2021-08-31 13:07:32 -0500

    tercero

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def notif(tittle, body):
     toaster.show_toast(tittle, body)
 
 def open_in_browser(url):
+    time.sleep(5)
     webbrowser.open(url)
 
 def get_date():
@@ -21,25 +22,18 @@ def get_date():
 
 def read_json_file(filename):
     myFile = open(filename)
-    urlFile = open('classesURL.json')
     data = json.load(myFile)
     date = get_date()
 
     for dayInstance in data:
         if dayInstance['day'] == date['day']:
             for myClass in dayInstance['clases']:
-                for classInstance in urlFile:
-                    if myClass['start'] == date['time']:
-                        notif(myClass['nombre'], 'La clase empieza {}\nLa clase temina {}\nSuerte!'.format(myClass['start'], myClass['end']))
-                        time.sleep(5)
-                        if classInstance['name'] == myClass['name']:
-                            open_in_browser(classInstance['url'])
-                        print(myClass['nombre'])
-                        time.sleep(65)
-                    elif myClass['end'] == date['time']:
-                        notif('Ha termidado la clase')
-                        print("End")
-                        time.sleep(65)
+                if myClass['start'] == date['time']:
+                    open_in_browser(myClass['url'])
+                    notif(myClass['name'], 'La clase empieza {}\nLa clase temina {}\nSuerte!'.format(myClass['start'], myClass['end']))
+                    print(myClass['name'])
+                    time.sleep(65)
+
 
     myFile.close()
 
